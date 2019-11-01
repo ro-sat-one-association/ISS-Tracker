0428a8377d388f28cb763667a0e3218f491a81e1
GigaCloud <radu.craciun420@gmail.com>
2019-11-01 16:00:23 +0000

    merge ok si pe pi

--- python/pi_n2yo.py
+++ python/pi_n2yo.py
@@ -6,11 +6,20 @@ import ephem
 import time
 import serial
 import sys
+import serial.tools.list_ports
+
+
 key = "BMDSE6-VEC9T4-Y6YANC-47A9"
 sat_code = "25544" #ISS 25544
 lat = "46.9438779"
 lon = "26.3534007"
 alt = "359"
+
+port = "/dev/"
+ports = list(serial.tools.list_ports.comports())
+for p in ports:
+    if "UART" in p.description:
+	port += p.name
 
 degrees_per_radian = 180.0 / math.pi
 home = ephem.Observer()
@@ -30,12 +39,6 @@ urltle = "https://www.n2yo.com/rest/v1/satellite/tle/25544&apiKey="
 urltle += key
 #print urltle
 
-def send(toSend):
-	port = "/dev/ttyUSB" + str(sys.argv[1])
-	ser = serial.Serial(port, 9600, timeout=0)
-	ser.write(str(toSend))
-	ser.close()
-
 def getAE():
 	response = urllib2.urlopen(urlpos)
 	data = json.loads(response.read())
@@ -52,6 +55,10 @@ def getTLE():
 tle = getTLE()
 iss = ephem.readtle('ISS', tle[0], tle[1])
 
+
+#port = "/dev/ttyUSB" + str(sys.argv[1])
+ser = serial.Serial(port, 9600, timeout=0)
+
 while True:
 	home.date = datetime.utcnow()
 	iss.compute(home)
@@ -62,7 +69,6 @@ while True:
 		elevationTLE = 0
 		
 	sendstr = "<" + str(int(azimuthTLE)) + " " + str(int(elevationTLE)) + ">"
-	sendstr = "<-2312 -2334>"
-	send(sendstr)
+	ser.write(sendstr)
 	print sendstr
 	time.sleep(1.0)
