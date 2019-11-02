055005bd2ea9f45f857eb06267310309dbdcb415
GigaCloud <radu.craciun420@gmail.com>
2019-11-02 21:08:14 +0200

    viteza smart af

--- python/n2yo.py
+++ python/n2yo.py
@@ -6,11 +6,20 @@ import ephem
 import time
 import serial
 import sys
+import serial.tools.list_ports
+
+
 key = "BMDSE6-VEC9T4-Y6YANC-47A9"
 sat_code = "25544" #ISS 25544
 lat = "46.9438779"
 lon = "26.3534007"
 alt = "359"
+
+port = "/dev/"
+ports = list(serial.tools.list_ports.comports())
+for p in ports:
+    if "UART" in p.description:
+    	port = port + p.name
 
 degrees_per_radian = 180.0 / math.pi
 home = ephem.Observer()
@@ -31,35 +40,44 @@ urltle += key
 #print urltle
 
 def getAE():
-	response = urllib2.urlopen(urlpos)
-	data = json.loads(response.read())
-	azimuth   = data["positions"][0]["azimuth"]
-	elevation = data["positions"][0]["elevation"]
-	return [azimuth, elevation]
+    response = urllib2.urlopen(urlpos)
+    data = json.loads(response.read())
+    azimuth   = data["positions"][0]["azimuth"]
+    elevation = data["positions"][0]["elevation"]
+    return [azimuth, elevation]
 
 def getTLE():
-	response = urllib2.urlopen(urltle)
-	data = json.loads(response.read())
-	tle = data["tle"].split("\r\n")
-	return tle
+    response = urllib2.urlopen(urltle)
+    data = json.loads(response.read())
+    tle = data["tle"].split("\r\n")
+    return tle
 
 tle = getTLE()
 iss = ephem.readtle('ISS', tle[0], tle[1])
 
 
-port = "/dev/ttyUSB" + str(sys.argv[1])
+#port = "/dev/ttyUSB" + str(sys.argv[1])
 ser = serial.Serial(port, 9600, timeout=0)
 
-while True:
-	home.date = datetime.utcnow()
-	iss.compute(home)
+lastE = 0
+lastA = 0
 
-	azimuthTLE   = iss.az  * degrees_per_radian
-	elevationTLE = iss.alt * degrees_per_radian
-	if elevationTLE < 0:
-		elevationTLE = 0
-		
-	sendstr = "<" + str(int(azimuthTLE)) + " " + str(int(elevationTLE)) + ">"
-	ser.write(sendstr)
-	print sendstr
-	time.sleep(1.0)
+while True:
+    home.date = datetime.utcnow()
+    iss.compute(home)
+
+    azimuthTLE   = iss.az  * degrees_per_radian
+    elevationTLE = iss.alt * degrees_per_radian
+    azimuthTLE   = int(azimuthTLE)
+    elevationTLE = int(elevationTLE)
+
+    if(elevationTLE < 0):
+        elevationTLE += 360
+    
+    if((lastE != elevationTLE) or (lastA != azimuthTLE)):
+        lastE = elevationTLE
+        lastA = azimuthTLE
+        sendstr = "<" + str(azimuthTLE) + " " + str(elevationTLE) + ">"
+        ser.write(sendstr)
+    	print sendstr
+    time.sleep(1.0)
