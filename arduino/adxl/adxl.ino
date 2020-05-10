9dda68c208fa540d96bbdcb8153a7ab692cbc142
GigaCloud <radu.craciun420@gmail.com>
2020-05-10 08:40:56 +0300

    adxl

---
+++ arduino/adxl/adxl.ino
@@ -0,0 +1,443 @@
+#include <Wire.h>
+#include "ADXL345.h"
+#include "HMC5883L_Simple.h"
+#include "DualVNH5019MotorShield.h"
+
+DualVNH5019MotorShield md;
+HMC5883L_Simple Compass;
+ADXL345 accelerometer;
+
+#define TOLERANTA_ELEVATIE 1.0f //in grade
+#define TOLERANTA_AZIMUTH  2.0f
+#define SENS_0_E 0             //pe astea le inversezi dupa teste, daca e nevoie
+#define SENS_1_E 1
+#define SENS_0_A 0             //pe astea le inversezi dupa teste, daca e nevoie
+#define SENS_1_A 1
+
+#define ELE_A_PIN 4
+#define ELE_B_PIN 3
+
+#define AZI_A_PIN 7
+#define AZI_B_PIN 8
+
+#define AZI_PWM 9
+#define ELE_PWM 10
+
+#define ELE_EN 6
+#define AZI_EN  12
+
+#define MIN_E 50 //puterea minima pwm  
+#define MIN_A 40
+
+#define K_E 10 //cu cate grade inainte sa incetinesc miscarea
+#define K_A 25
+
+#define MAX_E 255
+#define MAX_A 255
+
+bool debug;
+long long lastTime;
+char unroll_state;
+float initUnrollAngle;
+float heading;
+float pitch;
+float azimuth;
+float elevation;
+
+void initCompass() {
+  // The declination for your area can be obtained from http://www.magnetic-declination.com/
+  // Piatra-Neamt, 6°  15' EST
+  Compass.SetDeclination(6, 15, 'E');
+
+  Compass.SetSamplingMode(COMPASS_SINGLE);
+  Compass.SetScale(COMPASS_SCALE_130);
+  Compass.SetOrientation(COMPASS_HORIZONTAL_X_NORTH);
+}
+
+void setup(void) 
+{
+  Serial.begin(9600);
+  Serial.setTimeout(50);
+  azimuth = 0.0f;
+  elevation = 0.0f;
+
+  unroll_state = -1;
+
+  debug = false;
+  lastTime = 0;
+
+  pinMode(ELE_EN, OUTPUT);
+  pinMode(AZI_EN,  OUTPUT);
+
+  pinMode(ELE_A_PIN, OUTPUT);
+  pinMode(ELE_B_PIN, OUTPUT);
+
+  pinMode(AZI_A_PIN, OUTPUT);
+  pinMode(AZI_B_PIN, OUTPUT);
+
+  pinMode(AZI_PWM, OUTPUT);
+  pinMode(ELE_PWM, OUTPUT);
+
+  digitalWrite(AZI_EN,  HIGH);
+  digitalWrite(ELE_EN,  HIGH);
+
+  Wire.begin();
+
+  md.init();
+  if(!accelerometer.begin()){Serial.println("Nu merge AXL"); while(true){}}
+  accelerometer.setRange(ADXL345_RANGE_16G);
+  initCompass();
+}
+
+void moveAzimuth(bool sens, int putere)
+{
+  if (sens) {
+    md.setM2Speed(putere * 80 / 51);
+  } else {
+    md.setM2Speed(-1 * putere * 80 / 51);  
+  }
+}
+
+void moveElevation(bool sens, int putere)
+{
+  if (sens) {
+    md.setM1Speed(putere * 80 / 51);
+  } else {
+    md.setM1Speed(-1 * putere * 80 / 51);  
+  }
+}
+
+void stopAzimuth()
+{
+  digitalWrite(AZI_A_PIN, LOW);
+  digitalWrite(AZI_B_PIN, LOW);
+}
+
+void stopElevation()
+{
+  digitalWrite(ELE_A_PIN, LOW);
+  digitalWrite(ELE_B_PIN, LOW);
+}
+
+
+int checkSum(String t) {
+  unsigned int s = 0;
+  for (int i = 0; t[i] != '\0'; ++i) {
+    s += (int)t[i];
+  }
+  return s % 10;
+}
+
+bool validPackage(String textPacket) { //SPAGHETTI CODE, stiu
+  bool et = false;
+  String C = "\0";
+  String P = "\0";
+  if (textPacket[0] == '!') {
+    P += '!';
+    int i = 1;
+    for (i; textPacket[i] != '!'; ++i) {
+      if (textPacket[i] == '\0') return false;
+      P += textPacket[i];
+      if (textPacket[i] == '&') et = true;
+    }
+    if (!et) return false;
+    ++i;
+    if (textPacket[i] == '\0') return false;
+    for (i; textPacket[i] != '\0'; ++i) {
+      C += textPacket[i];
+    }
+    if (debug) {
+      Serial.print("ctoint ");
+      Serial.println(C.toInt());
+      Serial.print("csum ");
+      Serial.println(checkSum(P));
+    }
+    if (C.toInt() == checkSum(P) && et) return true;
+  }
+  return false;
+}
+
+
+void readData(float &azi, float &ele)
+{
+  String textPacket = "\0";
+  String A = "\0";
+  String E = "\0";
+
+  if (Serial.available()) {
+    textPacket =  Serial.readString();
+    textPacket.trim();
+    if (textPacket == "D") {
+      debug = !debug;
+      return;
+    }
+
+    if (textPacket == "A0") {
+      unroll_state = 1;
+      initUnrollAngle = heading;
+      return;
+    }
+    if (textPacket == "A1") {
+      unroll_state = 2;
+      initUnrollAngle = heading;
+      return;
+    }
+    if (textPacket == "E0") {
+      unroll_state = 3;
+      initUnrollAngle = pitch;
+      return;
+    }
+    if (textPacket == "E1") {
+      initUnrollAngle = pitch;
+      unroll_state = 4;
+      return;
+    }
+
+    if (textPacket[0] == 'A' && textPacket[1] == 'Z') { //AZ239.0 EL3.0 UP000 XXX DN000 XXX
+      int i = 2;
+      for (i; textPacket[i] != ' ' ; ++i) {
+        A += textPacket[i];
+      }
+      i += 3;
+      for (i; textPacket[i] != ' '; ++i) {
+        E += textPacket[i];
+      }
+      azi = A.toFloat();
+      ele = E.toFloat();
+      if (debug) {
+        Serial.print("Easycomm! Azimut:");
+        Serial.print(azi);
+        Serial.print(" Elevatie:");
+        Serial.println(ele);
+      }
+      return;
+    }
+
+    if (validPackage(textPacket)) {
+      unroll_state = -1;
+      int i = 1;
+      for (i; textPacket[i] != '&' ; ++i) {
+        A += textPacket[i];
+      }
+      i += 1;
+      for (i; textPacket[i] != '\0'; ++i) {
+        E += textPacket[i];
+      }
+      azi = A.toFloat();
+      ele = E.toFloat();
+      if (debug) {
+        Serial.print("Pachet valid! Azimut:");
+        Serial.print(azi);
+        Serial.print(" Elevatie:");
+        Serial.println(ele);
+      }
+    } else {
+      if (debug) {
+        Serial.println("Pachet nevalid");
+      }
+      return;
+    }
+  } else {
+    return;
+  }
+}
+
+float deltaAzimuth(float t, float h) {
+  if (fabs(t - h) < 180)
+    return fabs(t - h);
+  else
+    return 360 - fabs(t - h);
+}
+
+float deltaElevatie(float t, float r) {
+  if (r < 0) r += 360.0f;
+  if (t < 0) t += 360.0f;
+  return deltaAzimuth(t, r);
+}
+
+bool sensAzimuth(int t, int h) {
+  if (fabs(t - h) < 180) {
+    if (h > t)
+      return SENS_0_A;
+    else
+      return SENS_1_A;
+  } else {
+    if (h > t)
+      return SENS_1_A;
+    else
+      return SENS_0_A;
+  }
+}
+
+bool sensElevatie(int t, int r) {
+  if (r < 0) r += 360;
+  if (t < 0) t += 360;
+  if (fabs(t - r) < 180) {
+    if (r > t)
+      return SENS_0_E;
+    else
+      return SENS_1_E;
+  } else {
+    if (r > t)
+      return SENS_1_E;
+    else
+      return SENS_0_E;
+  }
+}
+
+int putereElevatie(int d) {
+  if (d > K_E) {
+    return MAX_E;
+  } else {
+    int v = MIN_E + (MAX_E - MIN_E) * d / K_E;
+    return v;
+  }
+}
+
+int putereAzimuth(int d) {
+  if (d > K_A) {
+    return MAX_A;
+  } else {
+    int v = MIN_A + (float)(MAX_A - MIN_A) * (float)(d) / (float)(K_A);
+    return v;
+  }
+}
+
+template <typename type>
+type sign(type value) {
+  return type((value > 0) - (value < 0));
+}
+
+void alignAzimuth(float t, float h) {
+  float delta = deltaAzimuth(t, h);
+  /*Serial.print(delta);
+    Serial.print('\t');
+    Serial.println(TOLERANTA_AZIMUTH);*/
+
+  if (delta > TOLERANTA_AZIMUTH) {
+    //digitalWrite(AZI_EN, HIGH);
+    moveAzimuth(sensAzimuth(t, h), putereAzimuth(delta));
+
+  } else {
+   // digitalWrite(AZI_EN, LOW);
+    stopAzimuth();
+  }
+}
+
+void alignElevation(float t, float r) {
+  float delta = deltaElevatie(t, r);
+  /*Serial.print(delta);
+    Serial.print('\t');
+    Serial.println(TOLERANTA_ELEVATIE);*/
+  if (delta > TOLERANTA_ELEVATIE) {
+    //digitalWrite(ELE_EN, HIGH);
+    moveElevation(sensElevatie(t, r), putereElevatie(delta));
+  } else {
+    stopElevation();
+    //digitalWrite(ELE_EN, LOW);
+  }
+}
+
+void getCompass() {
+  heading = Compass.GetHeadingDegrees();
+}
+
+void getPitch() {
+  Vector norm = accelerometer.readNormalize();
+  Vector filtered = accelerometer.lowPassFilter(norm, 0.5);
+  pitch = -(atan2(filtered.XAxis, sqrt(filtered.YAxis*filtered.YAxis + filtered.ZAxis*filtered.ZAxis))*180.0)/M_PI;
+}
+
+#define PRINT_DELAY 500
+
+void loop()
+{
+  getPitch();
+  getCompass();
+  readData(azimuth, elevation);
+  
+  if (millis() - lastTime > PRINT_DELAY) {
+    Serial.print(heading);
+    Serial.print(" ");
+    Serial.println(pitch);
+    lastTime = millis();
+    if(debug){
+      Serial.print("Heading: ");
+      Serial.println(heading);
+
+      Serial.print("Pitch:");
+      Serial.println(pitch);
+      Serial.println("-------------");
+    }
+  }
+
+  if (unroll_state != -1) {
+    switch (unroll_state) {
+      case 0: {
+          stopElevation();
+          stopAzimuth();
+          if (debug) {
+            Serial.println("Done unrolling");
+          }
+        } break;
+      case 1: { //A0
+          float delta = deltaAzimuth(initUnrollAngle, heading + 30.0f);
+          if (delta > 5.0f) {
+            alignAzimuth(heading + 30.0f, heading);
+            stopElevation();
+            if (debug) {
+              Serial.println("Unrolling... A0");
+            }
+          } else {
+            stopAzimuth();
+            unroll_state = 0;
+          }
+          // Serial.println("A0");
+        } break;
+
+      case 2: { //A1
+          float delta = deltaAzimuth(initUnrollAngle, heading - 30.0f);
+          if (delta > 5.0f) {
+            alignAzimuth(heading - 30.0f, heading);
+            stopElevation();
+          } else {
+            stopAzimuth();
+            unroll_state = 0;
+          }
+          // Serial.println("A1");
+
+        } break;
+      case 3: {
+          float delta = deltaElevatie(initUnrollAngle, pitch + 10.0f);
+          if (delta > 5.0f) {
+            alignElevation(pitch + 10.0f, pitch);
+            stopAzimuth();
+          } else {
+            stopElevation();
+            unroll_state = 0;
+          }
+          // Serial.println("E0");
+        } break;
+      case 4: {
+          float delta = deltaElevatie(initUnrollAngle, pitch - 10.0f);
+          if (delta > 5.0f) {
+            alignElevation(pitch - 10.0f, pitch);
+            stopAzimuth();
+          } else {
+            stopElevation();
+            unroll_state = 0;
+          }
+          //Serial.println("E1");
+        } break;
+
+    }
+  } else {
+    alignAzimuth(azimuth, heading);
+    alignElevation(elevation, pitch);
+  }
+
+  // rutinaTure();
+
+  //stopAzimuth();
+  //stopElevation();
+}
